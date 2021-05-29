dd3e48adc953ef0c69617e2bfc163f94703a1157
Mukesh Joshi <mukesh.bciit@gmail.com>
2021-05-29 14:39:57 +0200

    Minor refactoring

--- app/src/main/java/com/notifire/android/firebase/FireMsgService.java
+++ app/src/main/java/com/notifire/android/firebase/FireMsgService.java
@@ -1,17 +1,20 @@
 package com.notifire.android.firebase;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.notifire.android.MainActivity;
 import com.notifire.android.R;
 import com.notifire.android.adduser.RegisterToken;
-import com.notifire.android.authentication.SignupActivity;
-import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class FireMsgService extends FirebaseMessagingService implements RegisterToken {
@@ -19,29 +22,33 @@ public class FireMsgService extends FirebaseMessagingService implements Register
     private final String TAG = getClass().getSimpleName();
 
     @Override
-    public void onNewToken(@NotNull String token) {
+    public void onNewToken(@NonNull String token) {
         Log.d(TAG, "Refreshed token: " + token);
         sendRegistrationToServer(token);
     }
 
     @Override
-    public void onMessageReceived(@NotNull RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         Log.d("Msg", "Message received [" + remoteMessage + "]");
 
-        // Create Notification
-        Intent intent = new Intent(this, SignupActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1410, intent, PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.project_id))
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Message")
+                .setContentTitle(Objects.requireNonNull(remoteMessage.getNotification()).getTitle())
                 .setContentText(Objects.requireNonNull(remoteMessage.getNotification()).getBody())
                 .setAutoCancel(true)
+                .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1410, notificationBuilder.build());
+        NotificationChannel channel = new NotificationChannel(channelId, Objects.requireNonNull(remoteMessage.getNotification()).getTitle(), NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
